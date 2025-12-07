c7c0959e97cf6aaa21c413ee7a5e018122d06f8a
plaguemarine <calumrl@outlook.com>
2025-12-07 19:27:35 +0000

    Day 7 part 1 not finished need to test

--- 2025/Day7/Part1/Main.java
+++ 2025/Day7/Part1/Main.java
@@ -10,7 +10,8 @@ public class Main
     public  static void main(String[] args)
     {
 
-        Character[][] tachyonManifold = new Character[16][15];
+        //Character[][] tachyonManifold = new Character[16][15];
+        Character[][] tachyonManifold = new Character[142][141];
         ArrayList<Integer> tachyonBeamsIdex = new ArrayList<Integer>();
 
         try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/Calum/IdeaProjects/Advent-of-Code-2025/2025/Day7/Part1/input.txt"))) {
@@ -30,8 +31,6 @@ public class Main
             System.out.println("Error reading file.");
         }
 
-        int size = tachyonBeamsIdex.size();
-
         for (int tachyonStart = 0; tachyonStart < tachyonManifold[0].length; tachyonStart++)
         {
             if (tachyonManifold[0][tachyonStart] == 'S')
@@ -44,31 +43,57 @@ public class Main
         int numBeams = 1;
         int splitterCounter = 0;
 
-        ArrayList<Integer> temptachyonBeamsIdex = new ArrayList<Integer>();
-
         for (int rows = 1; rows < tachyonManifold.length; rows++)
         {
-            for (int i = 0; i < tachyonManifold.length; i++)
+            for (int col = 0; col < tachyonManifold[rows].length; col++)
             {
-                if (tachyonManifold[rows][tachyonBeamsIdex.get(i)] == '.')
+                if (tachyonManifold[rows][col] == '^')
                 {
-                    continue;
+                    if (tachyonBeamsIdex.contains(col))
+                    {
+                        splitterCounter++;
+                        for (int i = 0; i < tachyonBeamsIdex.size(); i++)
+                        {
+                            if (tachyonBeamsIdex.contains(col))
+                            {
+                                if (tachyonBeamsIdex.get(i) == col)
+                                {
+                                    tachyonBeamsIdex.remove(i);
+                                    tachyonBeamsIdex.add(col - 1);
+                                    tachyonBeamsIdex.add(col + 1);
+                                    System.out.println("splitterfound " + splitterCounter);
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
                 }
-                else if (tachyonManifold[rows][tachyonBeamsIdex.get(i)] == '^')
-                {
-                    System.out.println("splitterfound");
-                    splitterCounter++;
+            }
 
-                    temptachyonBeamsIdex.add(tachyonBeamsIdex.get(i) + 1);
-                    temptachyonBeamsIdex.add(tachyonBeamsIdex.get(i) - 1);
-                }
-            }
-            tachyonBeamsIdex.remove(tachyonBeamsIdex);
-            for (int i = 0; i < temptachyonBeamsIdex.size(); i++)
-            {
-                tachyonBeamsIdex.add(temptachyonBeamsIdex.get(i));
-            }
-            temptachyonBeamsIdex.remove(temptachyonBeamsIdex);
+//            for (int i = 0; i < tachyonBeamsIdex.size(); i++)
+//            {
+//                if (tachyonManifold[rows][tachyonBeamsIdex.get(i)] == '.')
+//                {
+//                    continue;
+//                }
+//                else if (tachyonManifold[rows][tachyonBeamsIdex.get(i)] == '^')
+//                {
+//                    System.out.println("splitterfound");
+//                    splitterCounter++;
+//
+//                    temptachyonBeamsIdex.add(tachyonBeamsIdex.get(i) + 1);
+//                    temptachyonBeamsIdex.add(tachyonBeamsIdex.get(i) - 1);
+//                }
+//            }
+//            tachyonBeamsIdex.clear();
+//            for (int i = 0; i < temptachyonBeamsIdex.size(); i++)
+//            {
+//                tachyonBeamsIdex.add(temptachyonBeamsIdex.get(i));
+//            }
+//            temptachyonBeamsIdex.remove(temptachyonBeamsIdex);
         }
 
         System.out.println("Number of times split " + splitterCounter);
